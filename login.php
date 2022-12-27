6685b14c88a7b67318698467b13bf18b69cec7cb
Christian <c.ristian-lp@hotmail.com>
2022-12-27 10:23:33 -0500

    Se crea carpeta BD que contiene informacion sobre la creacion BD

--- login.php
+++ login.php
@@ -11,24 +11,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <script src="JS/JqueryLib.js"></script>
-    <script>
-        $(document).ready(function(){
-            $("#ingresar").click(function(){
-                var usuario = $("#usuario").val();
-                var clave = $("#clave").val();
-
-                $.post("validar.php",{
-                    u: usuario,
-                    c:clave
-                },
-                function(data, status){
-                    if(data == 1){
-                        location.href = 'index.php';
-                    }
-                });
-            });
-        });
-    </script>
 </head>
 <body>
     
