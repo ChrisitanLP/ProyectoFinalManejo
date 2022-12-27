51dac66a7c308cf2ea874c258d47b599ab81136f
Christian <c.ristian-lp@hotmail.com>
2022-12-27 12:32:00 -0500

    Modificacion archivos, recursos y paginas

--- login.php
+++ login.php
@@ -1,7 +1,9 @@
 <?php
+
     session_start();
     if(isset($_SESSION['u']))
-        header("Location: index.html");
+        header("Location: index.php");
+        
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,25 +17,6 @@
     <link rel="stylesheet" type="text/css" href="CSS/styleLogin.css">
 
     <script src="JS/JqueryLib.js"></script>
-    <script>
-        $(document).ready(function(){
-            $("#ingresar").click(function(){
-                var usuario = $("#usuario").val();
-                var clave = $("#clave").val();
-                
-                $.post("Conexion/validar.php",{
-                    u: usuario,
-                    c: clave
-                },
-                function(data, status){
-                    if(data==1){
-                        location.href = 'index.html';
-                    }
-                });
-            });
-        });
-    </script>
-
 </head>
 <body>
     <div id="login-bg" class="container-fluid">
