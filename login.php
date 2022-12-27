8e1dc9cf2a3224ae261b429b529fa881354aea46
Christian <c.ristian-lp@hotmail.com>
2022-12-27 10:56:22 -0500

    Se crea una carpeta CSS (almacena archivos de estilo) y Recursos (almacena imagenes)

--- login.php
+++ login.php
@@ -10,9 +10,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Loding font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,700" rel="stylesheet">
+
+    <!-- Custom Styles -->
+    <link rel="stylesheet" type="text/css" href="CSS/styleLogin.css">
+
     <script src="JS/JqueryLib.js"></script>
 </head>
 <body>
-    
+    <div id="login-bg" class="container-fluid">
+
+    <div class="bg-img"></div>
+    <div class="bg-color"></div>
+    </div>
+
+    <!-- End Backgrounds -->
+
+    <div class="container" id="login">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="login">
+
+                    <h1>Login</h1>
+                    <form>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="usuario" aria-describedby="emailHelp" placeholder="Ingrese Usuario">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="clave" placeholder="Ingrese Contraseña">
+                        </div>
+
+                        <div class="form-check">
+                            <label class="switch">
+                                <input type="checkbox">
+                                <span class="slider round"></span>
+                            </label>
+                            <label class="form-check-label" for="exampleCheck1">Recordar Contraseña</label>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-lg btn-block btn-success" id="ingresar">Enviar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
