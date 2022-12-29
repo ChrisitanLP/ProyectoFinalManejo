e1991048fa5e7376b3e54945f3a56a5832fd6c8c
Esteban <pedro@example.com>
2022-12-29 15:46:49 -0500

    Se modifico el archivo validar.php

--- Conexion/validar.php
+++ Conexion/validar.php
@@ -1,27 +1,31 @@
 <?php
-    //echo "Hola Mundo";
-    
-    if(isset($_SESSION)){
+     if(isset($_SESSION)){
         $_SESSION['u']="";
     }else{
         session_start();
         $_SESSION['u']="";
     }
-    
-
     include_once("conectar.php");
+    $conexion=mysqli_connect("localhost","root","","manejo");
+    $usuario=$_POST['usuario'];
+    $contraseña=$_POST['contraseña'];
+    //session_start();
+    $_SESSION['usuario']=$usuario;
+    $consulta="SELECT*FROM LOGIN where USU_LOG='$usuario' and PAS_LOG='$contraseña'";
+    $resultado=mysqli_query($conexion,$consulta);
     
-    if(isset($_POST['u'])){
-        $con = conectar();
-        $query = "SELECT COUNT(*)cantidad FROM login WHERE USU_LOG = ? AND PAS_LOG = ? ";
-        $sentencia = $con -> prepare($query);
-        $sentencia -> execute(array($_POST['u'],$_POST['c']));
-        $r = $sentencia -> fetch();
-        
-        if($r['cantidad']==1){
-            $_SESSION['u']= $_POST['u'];
-        }
-
-        echo $r['cantidad'];
+    $filas=mysqli_fetch_array($resultado);
+    
+    if($filas['ROL_LOG']=="Administrador"){ //administrador
+        header("location:../Paginas/pag_admin.php");
+    
+    }else
+    if($filas['ROL_LOG']=="Docente"){ //cliente
+    header("location:../Paginas/pag_docentes.php");
     }
+    else{
+        header("location:../login.php"); 
+    }
+    mysqli_free_result($resultado);
+    mysqli_close($conexion);
 ?>
